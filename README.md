8f4fddc50ea546cf3d024d16800560629fb6f83c
Paolo Martini <paolo.cavei@gmail.com>
2020-05-18 17:24:51 +0200

    New README

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
-# GUESS LOI PREPARE
+# BrewerIX prepare knowledge-base
 
-Download and format data for Brew-loi.
+Download and format data for brewerix-cli.
+
+## Quick start
+Generate a knowledge-base for brewerix-cli is as simple as running
+
+build_knowledgebase -e 98 homo_sapiens chromosomes.txt imprinted_genes.txt
+
+were chromosomes.txt is the list of chromosomes and 
+imprinted_genes.txt is the list of imprinted gene Hugo Symbols.
+
+
--- setup.py
+++ setup.py
@@ -1,12 +1,11 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="guess-loi-prepare",
-    version="0.2.0",
+    name="brewerix-prepare-knowledgebase",
+    version="0.3.0",
     author="Romualdi's Lab",
     author_email=[
         "paolo.cavei@gmail.com",
-        "enrica.calura@gmail.com",
         "gbrsales@gmail.com",
     ],
     description="Tools to prepare data for Guess-LOI",
@@ -18,14 +17,14 @@ setup(
     entry_points={
         'console_scripts': [
             'build_knowledgebase=guess_loi_prep.build_knowledgebase:main',
-            'download_annotation=guess_loi_prep.commands.annotation:main',
-            'download_genome=guess_loi_prep.download_genome:main',
-            'download_chromosome_variants=guess_loi_prep.download_variants:main',
-            'filter_gtf=guess_loi_prep.filter_gtf:filter_gtf',
-            'filter_vcfs=guess_loi_prep.filter_vcf:filter_vcfs',
-            'split_variants=guess_loi_prep.variant_selection:create_vcf_for_brew_loi',
-            'create_geneimprint_annotation=guess_loi_prep.create_geneimprint_annotation:create_geneimprint_annotation',
-            'create_genetype_annotation=guess_loi_prep.create_genetype_annotation:create_genetype_annotation'
+            # 'download_annotation=guess_loi_prep.commands.annotation:main',
+            # 'download_genome=guess_loi_prep.download_genome:main',
+            # 'download_chromosome_variants=guess_loi_prep.download_variants:main',
+            # 'filter_gtf=guess_loi_prep.filter_gtf:filter_gtf',
+            # 'filter_vcfs=guess_loi_prep.filter_vcf:filter_vcfs',
+            # 'split_variants=guess_loi_prep.variant_selection:create_vcf_for_brew_loi',
+            # 'create_geneimprint_annotation=guess_loi_prep.create_geneimprint_annotation:create_geneimprint_annotation',
+            # 'create_genetype_annotation=guess_loi_prep.create_genetype_annotation:create_genetype_annotation'
         ],
     }
 )
